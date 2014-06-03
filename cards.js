67282a9264d3ee0ce30e874eb7e2ab0b6b548c25
Tom McCluskey <bosun.tom@gmail.com>
2014-06-03 15:58:50 -0700

    Attempt while leaving all functions inside makeCard object

--- cards.js
+++ cards.js
@@ -3,58 +3,42 @@
 // function()--> possible return values
 
 function makeCard(id) {
-  var newCard = {
-    var number = id; 
-    newCard.rank = makeCard.rank;
-    newCard.suit = makeCard.suit;
-    newCard.color = makeCard.color;
-    newCard.cardID = makeCard.cardID;
-    newCard.name = makeCard.name;
-    newCard.precedes = makeCard.precedes;
-    newCard.nextInSuit = makeCard.nextInSuit;
-    newCard.prevInSuit = makeCard.prevInSuit;
-    inRange: function (number, min, max) {
+  var newCard = { number: id,
+    inrange: function (number, min, max) {
         if (typeof (number) !== "number") {
-            console.error('Validation Error! "' + number + '" is not a number');
-            return NaN;
+            console.error('validation error! "' + number + '" is not a number');
+            return nan;
         } else if ((number <= max) && (number >= min)) {
             return true;
         } else {
-            console.error('Validation Error! "' + number + '" is outside the given range');
+            console.error('validation error! "' + number + '" is outside the given range');
             return undefined;
         }
-    },
-
-  }
-  
-}
-
-   newCard.rank = function (card) { // --> 1..13
-        if ( this.inRange(card, 0, 51)) {
+    } 
+    ,
+    rank: function (card) { // --> 1..13
+        if ( makeCard.inRange(card, 0, 51)) {
             return (Math.floor(card/4) + 1);
         } else {
             return;
         }        
-    }
-    
-    newCard.suit = function (card) { // --> 1..4
-        if ( this.inRange(card, 0, 51)) {
+    },
+    suit: function (card) { // --> 1..4
+        if ( makeCard.inRange(card, 0, 51)) {
             return ((card) % 4) + 1;
         } else {
             return;
         }        
-    }
-    
-    newCard.cardID = function (rank, suit) { // --> 0..51
-        if ((this.inRange(rank, 1, 13)) && (this.inRange(suit, 1, 4))) {
+    },
+    cardID: function (rank, suit) { // --> 0..51
+        if ((makeCard.inRange(rank, 1, 13)) && (makeCard.inRange(suit, 1, 4))) {
             return ((rank - 1) * 4 + suit - 1);
         } else {
             return;
         }        
-    }
-    
-    newCard.color = function (card) { // -->"red","black"
-        if ( this.inRange(card, 0, 51)) {
+    },
+    color: function (card) { // -->"red","black"
+        if ( makeCard.inRange(card, 0, 51)) {
             if (this.suit(card) < 3) {
                 return "red";
             } else {
@@ -63,10 +47,9 @@ function makeCard(id) {
         } else {
             return;
         }
-    }
-    
-    newCard.name = function (card) { // --> string
-            if (this.inRange(card, 0, 51)) {
+    },
+    cardName: function (card) { // --> string
+            if (makeCard.inRange(card, 0, 51)) {
             var nameString = "";
             switch (this.rank(card)) {
             case 1:
@@ -128,10 +111,9 @@ function makeCard(id) {
             } else {
                 return;
             }
-    }
-    
-    newCard.precedes = function (cardA, cardB) { //-->false,true
-       if ((this.inRange(cardA, 0, 51)) && (this.inRange(cardB, 0, 51))) {
+    },
+    precedes: function (cardA, cardB) { //-->false,true
+       if ((makeCard.inRange(cardA, 0, 51)) && (makeCard.inRange(cardB, 0, 51))) {
             if (this.rank(cardB) - this.rank(cardA)  ===  1) {
                 return true;
             } else if ((this.rank(cardB) == 1) && (this.rank(cardA) == 13)) {
@@ -142,10 +124,9 @@ function makeCard(id) {
        } else {
            return;
        }
-    }
-    
-    newCard.sameColor = function (cardA, cardB) { //-->false,true
-           if ((this.inRange(cardA, 0, 51)) && (this.inRange(cardB, 0, 51))) {
+    },
+    sameColor: function (cardA, cardB) { //-->false,true
+           if ((makeCard.inRange(cardA, 0, 51)) && (makeCard.inRange(cardB, 0, 51))) {
             if (this.color(cardB) == this.color(cardA)) {
                 return true;
             } else {
@@ -154,10 +135,9 @@ function makeCard(id) {
        } else {
            return;
        }
-        }
-    
-    newCard.nextInSuit = function (cardA) {//--> 0..51
-        if (this.inRange(cardA, 0, 51)) {
+        },
+    nextInSuit: function (cardA) {//--> 0..51
+        if (makeCard.inRange(cardA, 0, 51)) {
             if (cardA < 48) {
                 return (cardA + 4);
             } else {
@@ -167,10 +147,9 @@ function makeCard(id) {
     
             return;
         }        
-    }
-    
-    newCard.prevInSuit = function (cardB) {//--> 0..51
-        if (this.inRange(cardB, 0, 51)) {
+    },
+    prevInSuit: function (cardB) {//--> 0..51
+        if (makeCard.inRange(cardB, 0, 51)) {
             if (cardB > 3) {
                 return (cardB - 4);
             } else {
@@ -181,10 +160,19 @@ function makeCard(id) {
             return;
         }        
     }
+}
 
 
-// TESTING:
-var alias = cardReader;//change as needed
+    newCard.rank = makeCard.rank;
+    newCard.suit = makeCard.suit;
+    newCard.color = makeCard.color;
+    newCard.cardID = makeCard.cardID;
+    newCard.cardName = makeCard.cardName;
+    newCard.precedes = makeCard.precedes;
+    newCard.nextInSuit = makeCard.nextInSuit;
+    newCard.prevInSuit = makeCard.prevInSuit;
+};
+/*// TESTING:
 
 function assert(claim,message) {
     if (!claim) console.error(message);
@@ -234,4 +222,4 @@ assert((typeof alias.precedes(51, 52)) != 'boolean', "Test 38 failed");
 assert((typeof alias.precedes(-1, 0)), "Test 39 failed");
 assert((typeof alias.sameColor("0","1")) != 'boolean', "Test 40 failed");
 assert(isNaN(alias.nextInSuit(52)), "Test 41 failed");
-assert(isNaN(alias.prevInSuit(52)), "Test 42 failed");
+assert(isNaN(alias.prevInSuit(52)), "Test 42 failed");*/

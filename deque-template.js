35dbccc0d60736ae5c8c1fdd37580feffc351492
Tom McCluskey <bosun.tom@gmail.com>
2014-06-04 16:19:00 -0700

    Initial deque functions through unshift working; pop and push tests not working as expected

--- deque-template.js
+++ deque-template.js
@@ -1,58 +1,94 @@
 //-------
 // Part a): build a deque factory
 function makeDeque(values) {
-	// ...
+	var newDeque = {array: []}, i = 0;
+    for ( i; i < values.length; i++) {
+        newDeque.array[i] = values[i];
+    }
+    
+    newDeque.top = makeDeque.top;
+    newDeque.bottom = makeDeque.bottom;
+    newDeque.pop = makeDeque.pop;
+    newDeque.push = makeDeque.push;
+    newDeque.shift = makeDeque.shift;
+    newDeque.unshift = makeDeque.unshift;
+    newDeque.cut = makeDeque.cut;
+    newDeque.map = makeDeque.map;
+    newDeque.sort = makeDeque.sort;
+    
+    return newDeque;
+}
+
+makeDeque.top = function () {
+	return this.array[0];
+};
+makeDeque.bottom = function () {
+	return this.array[(this.array.length - 1)];
 };
 
-makeDeque.top = function() {
+makeDeque.pop = function () {
+	return this.array.pop();
+};
+makeDeque.push = function (val) {
+	this.array.push(val);
+    return;
+};
+
+makeDeque.shift = function () {
+	return this.array.shift();
+};
+makeDeque.unshift = function (val) {
+	this.array.unshift(val);
+    return;
+};
+
+makeDeque.cut = function (offset) {
 	//...
 };
-makeDeque.bottom = function() {
+
+makeDeque.map = function (convertValFn) {
 	//...
 };
 
-makeDeque.pop = function() {
-	//...
-};
-makeDeque.push = function(val) {
+makeDeque.sort = function (compareValsFn) {
 	//...
 };
 
-makeDeque.shift = function() {
-	//...
-};
-makeDeque.unshift = function(val) {
-	//...
-};
+//var someCards = /* make array of 52 card objects here, using your code from Problem 1) */;
+//// At this point, data looks like Fig.1
+//
+////-------
+//// Part b): build a deque instance:
+//var deckOfCards = makeDeque (someCards);
+//// sort it:
+//deckOfCards.sort (/* something here */);
+//// At this point, data looks like Fig.2
+//
+//// sort it differently:
+//deckOfCards.sort (/* something different here */);
+//
+////-------
+//// Part c): build another deque instance:
+//var someNames = /* make array of student/TA names here */;
+//var deckOfNames = makeDeque (someNames);
+//deckOfNames.sort (/* something here */);
 
-makeDeque.cut = function(offset) {
-	//...
-};
+//Testing!
 
-makeDeque.map = function(convertValFn) {
-	//...
-};
+var values = [1,2,3,4,5,6,7], x = makeDeque(values);
 
-makeDeque.sort = function(compareValsFn) {
-	//...
-};
+function assert(claim,message) {
+    if (!claim) console.error(message);
+}
 
-var someCards = /* make array of 52 card objects here, using your code from Problem 1) */;
-// At this point, data looks like Fig.1
+assert(x.array !== values, 'failed copy rather than alias test!');
+assert(x.top() === 1, 'failed top test!');
+assert(x.bottom() === 7, 'failed bottom test!');
+x.push(8);
+assert(x.array == [1,2,3,4,5,6,7,8], 'failed push test!');
+assert(x.pop() == 8, 'failed pop test!');
+assert(x.shift() == 1, 'failed shift test!');
+x.unshift(1);
+assert(x.array == [1,2,3,4,5,6,7], 'failed unshift test!');
 
-//-------
-// Part b): build a deque instance:
-var deckOfCards = makeDeque(someCards);
-// sort it:
-deckOfCards.sort(/* something here */);
-// At this point, data looks like Fig.2
-
-// sort it differently:
-deckOfCards.sort(/* something different here */);
-
-//-------
-// Part c): build another deque instance:
-var someNames = /* make array of student/TA names here */;
-var deckOfNames = makeDeque(someNames);
-deckOfNames.sort(/* something here */);
-
+console.log('Finished testing');
